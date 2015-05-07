ae70f8c0b538d6562610faa39ee140adbce1b14a
Daniel J Sissom <daniel.j.sissom@vanderbilt.edu>
2015-05-06 20:55:14 -0500

    Added boilerplate and matplotlib tweaks.

--- ellipsoids.py
+++ ellipsoids.py
@@ -1,1 +1,51 @@
+#!/usr/bin/env python
 
+import sys
+import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import bgc2
+from ipdb import set_trace
+
+
+
+def main():
+	pass
+
+
+
+def add_white_to_colormap(orig_map, num):
+	from matplotlib import cm
+	temp_cmap = cm.get_cmap(orig_map, num)
+	vals = temp_cmap(np.arange(num))
+	nfade = num / 7
+	vals[:nfade,0] = np.linspace(1., vals[nfade-1,0], nfade)
+	vals[:nfade,1] = np.linspace(1., vals[nfade-1,1], nfade)
+	vals[:nfade,2] = np.linspace(1., vals[nfade-1,2], nfade)
+	newcmap = mpl.colors.LinearSegmentedColormap.from_list("custom_1", vals)
+	return newcmap
+
+
+
+colormap = add_white_to_colormap('rainbow', 30)
+plot_dest_type = 'paper'
+if plot_dest_type == 'paper':
+	mpl.rcParams['font.family'] = 'serif'
+	mpl.rcParams['font.size'] = 16
+	mpl.rcParams['axes.linewidth'] = 3
+	mpl.rcParams['lines.linewidth'] = 4
+	mpl.rcParams['patch.linewidth'] = 4
+	mpl.rcParams['xtick.major.width'] = 3
+	mpl.rcParams['ytick.major.width'] = 3
+	mpl.rcParams['xtick.major.size'] = 8
+	mpl.rcParams['ytick.major.size'] = 8
+	mpl.rcParams['xtick.minor.width'] = 2
+	mpl.rcParams['ytick.minor.width'] = 2
+	mpl.rcParams['xtick.minor.size'] = 4
+	mpl.rcParams['ytick.minor.size'] = 4
+
+
+
+if __name__ == '__main__':
+	main()
